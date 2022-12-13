1715391aad26f9403a63af04163d0e3a73983c5d
GiadaGallitto <gallitto.giada@gmail.com>
2022-12-13 15:03:56 +0100

    Add click event with vue

--- js/script.js
+++ js/script.js
@@ -6,23 +6,23 @@ createApp({
             activeIndex : 0,
             imagesList : [
                 {
-                    image: 'img/01.webp',
+                    image: './img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                     }, {
-                        image: 'img/02.webp',
+                        image: './img/02.webp',
                         title: 'Ratchet & Clank: Rift Apart',
                         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                     }, {
-                        image: 'img/03.webp',
+                        image: './img/03.webp',
                         title: 'Fortnite',
                         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                     }, {
-                        image: 'img/04.webp',
+                        image: './img/04.webp',
                         title: 'Stray',
                         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                     }, {
-                        image: 'img/05.webp',
+                        image: './img/05.webp',
                         title: "Marvel's Avengers",
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                     }
@@ -37,6 +37,12 @@ createApp({
                 this.activeIndex = this.imagesList.length - 1;
             }
         },
-        
+
+        nextSlide() {
+            this.activeIndex++;
+            if(this.activeIndex >= this.imagesList.length) {
+                this.activeIndex = 0;
+            }
+        },
     }
-})
+}).mount(`#app`);
